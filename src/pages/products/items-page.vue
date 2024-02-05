cc43bef050a57ddc02263cad050749990d0e07c9
q0d1r0v08 <qodirovoybekjon08@gmail.com>
2024-02-05 14:09:46 +0500

    w-code

--- src/pages/products/items-page.vue
+++ src/pages/products/items-page.vue
@@ -4,13 +4,15 @@ import { useRoute } from "vue-router"
 import { ref, onMounted, watch, nextTick } from "vue"
 import { client } from "../../utils/axios";
 import { Loading, Notify } from "quasar";
+import { Icon } from "@iconify/vue"
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 // types
 interface ITypesOfItems {
     pagination: {
         page: number,
-        size: number
+        size: number,
+        max_page: number
     },
     data: any[]
 }
@@ -21,7 +23,8 @@ const route = useRoute()
 const data_of_items = ref<ITypesOfItems>({
     pagination: {
         page: 1,
-        size: 15
+        size: 15,
+        max_page: 1
     },
     data: []
 })
@@ -35,10 +38,13 @@ async function getProducts() {
             const { data } = await client.get("/api/products", {
                 params: {
                     is_active: true,
-                    search: route.query.name
+                    search: route.query.name,
+                    skip: data_of_items.value.pagination.page,
+                    // limit: data_of_items.value.pagination.size
                 }
             })
             data_of_items.value.data = data.data
+            data_of_items.value.pagination.max_page = data.pages
         } catch (err) {
             Notify.create({
                 color: "red",
@@ -53,10 +59,13 @@ async function getProducts() {
             const { data } = await client.get("/api/products", {
                 params: {
                     is_active: true,
-                    categoryId: route.query.category
+                    categoryId: route.query.category,
+                    skip: data_of_items.value.pagination.page,
+                    // limit: data_of_items.value.pagination.size
                 }
             })
             data_of_items.value.data = data.data
+            data_of_items.value.pagination.max_page = data.pages
         } catch (err) {
             Notify.create({
                 color: "red",
@@ -91,8 +100,15 @@ onMounted(() => {
 <template>
     <div class="mb-4 min-h-screen">
         <div v-if="data_of_items.data.length > 0">
-            <div class="mt-4 text-lg">
-                Qidiruv: {{ route.query.name }}
+            <div class="mt-4 text-lg flex justify-between">
+                <div>
+                    Qidiruv: {{ route.query.name }}
+                </div>
+
+                <div>
+                    <q-pagination input v-model="data_of_items.pagination.page" :max="data_of_items.pagination.max_page"
+                        :max-pages="6" @update:model-value="getProducts" />
+                </div>
             </div>
             <div class="mt-4 row">
                 <div class="max-sm:w-full min-h-[350px] col-sm-6 col-md-3 col-lg-2 p-4 bg-white border-8 border-[#f6f8fa] item"
@@ -114,6 +130,18 @@ onMounted(() => {
                     <span class="text-sm mt-4">
                         {{ item.name }}
                     </span>
+
+                    <div class="text-bold">
+                        {{ parseInt(item.price).toLocaleString().split(",").join(" ") }} UZS
+                    </div>
+                    <div class="mt-4">
+                        <q-btn unelevated outline style="color: #7316F6;">
+                            <div class="flex items-center gap-2">
+                                Sotib olish
+                                <Icon icon="ri-shopping-cart-fill" />
+                            </div>
+                        </q-btn>
+                    </div>
                 </div>
             </div>
         </div>
